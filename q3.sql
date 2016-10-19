068bbe3b1ecd41b13941e08405bfbbb9019a1be1
TeppieC <teppie.c@hotmail.com>
2016-10-19 15:08:24 -0600

    finish demo2

--- q3.sql
+++ q3.sql
@@ -1,3 +1,38 @@
+CREATE TABLE rtree(
+   id,              -- Integer primary key
+   minX, maxX,      -- Minimum and maximum X coordinate
+   minY, maxY       -- Minimum and maximum Y coordinate
+);
+
+INSERT INTO rtree(id, minX, maxX, minY, maxY)
+SELECT 1, 2, 5, 23, 25;
+INSERT INTO rtree(id, minX, maxX, minY, maxY)
+SELECT 2, 3, 7, 17, 20;
+INSERT INTO rtree(id, minX, maxX, minY, maxY)
+SELECT 3, 1, 4, 11, 13;
+INSERT INTO rtree(id, minX, maxX, minY, maxY)
+SELECT 4, 1, 4, 0, 3;
+INSERT INTO rtree(id, minX, maxX, minY, maxY)
+SELECT 5, 6, 9, 21, 24;
+INSERT INTO rtree(id, minX, maxX, minY, maxY)
+SELECT 6, 7, 9, 15, 20;
+INSERT INTO rtree(id, minX, maxX, minY, maxY)
+SELECT 7, 6, 13, 3, 8;
+INSERT INTO rtree(id, minX, maxX, minY, maxY)
+SELECT 8, 17, 20, 9, 22;
+INSERT INTO rtree(id, minX, maxX, minY, maxY)
+SELECT 9, 19, 24, 9, 12;
+INSERT INTO rtree(id, minX, maxX, minY, maxY)
+SELECT 10, 19, 23, 6, 8;
+INSERT INTO rtree(id, minX, maxX, minY, maxY)
+SELECT 11, 21, 26, 21, 25;
+INSERT INTO rtree(id, minX, maxX, minY, maxY)
+SELECT 12, 20,30,15,17;
+INSERT INTO rtree(id, minX, maxX, minY, maxY)
+SELECT 13, 25,28,12,16;
+INSERT INTO rtree(id, minX, maxX, minY, maxY)
+SELECT 14, 13,17,19,22;
+
 CREATE VIRTUAL TABLE rtree_index USING rtree(
    id,              -- Integer primary key
    minX, maxX,      -- Minimum and maximum X coordinate
@@ -5,30 +40,7 @@ CREATE VIRTUAL TABLE rtree_index USING rtree(
 );
 
 INSERT INTO rtree_index(id, minX, maxX, minY, maxY)
-SELECT 1, 2, 5, 23, 25;
-INSERT INTO rtree_index(id, minX, maxX, minY, maxY)
-SELECT 2, 3, 7, 17, 20;
-INSERT INTO rtree_index(id, minX, maxX, minY, maxY)
-SELECT 3, 1, 4, 11, 13;
-INSERT INTO rtree_index(id, minX, maxX, minY, maxY)
-SELECT 4, 1, 4, 0, 3;
-INSERT INTO rtree_index(id, minX, maxX, minY, maxY)
-SELECT 5, 6, 9, 21, 24;
-INSERT INTO rtree_index(id, minX, maxX, minY, maxY)
-SELECT 6, 7, 9, 15, 20;
-INSERT INTO rtree_index(id, minX, maxX, minY, maxY)
-SELECT 7, 6, 13, 3, 8;
-INSERT INTO rtree_index(id, minX, maxX, minY, maxY)
-SELECT 8, 17, 10, 9, 22;
-INSERT INTO rtree_index(id, minX, maxX, minY, maxY)
-SELECT 9, 19, 24, 9, 12;
-INSERT INTO rtree_index(id, minX, maxX, minY, maxY)
-SELECT 10, 19, 23, 6, 8;
-INSERT INTO rtree_index(id, minX, maxX, minY, maxY)
-SELECT 11, 21, 26, 21, 25;
-INSERT INTO rtree_index(id, minX, maxX, minY, maxY)
-SELECT 12, 20,30,15,17;
-INSERT INTO rtree_index(id, minX, maxX, minY, maxY)
-SELECT 13, 25,28,12,16;
-INSERT INTO rtree_index(id, minX, maxX, minY, maxY)
-SELECT 14, 13,17,19,22;
+SELECT id, minX, maxX, minY, maxY
+FROM rtree;
+
+-- using test2.db
